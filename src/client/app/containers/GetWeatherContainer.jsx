01670e1a7cee82d4913180c4a6fd759521554bbc
Alessandro <alessandro@playstudios.com>
2016-08-02 19:35:32 -0700

    Added handling for weather api

--- src/client/app/containers/GetWeatherContainer.jsx
+++ src/client/app/containers/GetWeatherContainer.jsx
@@ -1,17 +1,44 @@
 import React, {PropTypes} from 'react';
 import {getWeatherData} from '../helpers/WeatherApiHelper';
+import moment from 'moment';
+
+const ListItemWrapper = (props) => <li>{props.text}</li>
 
 export default class GetWeatherContainer extends React.Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      locationName: '',
+      weatherData: []
+    }
   }
 
   componentDidMount() {
-    getWeatherData(this.props.routeParams.location);
+    getWeatherData(this.props.routeParams.location)
+      .then((weatherObj) => {
+        console.log(weatherObj);
+        this.setState({
+            locationName: weatherObj.cityName,
+            weatherData: weatherObj.weatherData
+        })
+      });
   }
 
   render() {
-    return (<div>MyComponent</div>);
+    return (
+      <div className='text-center'>
+        <h1>{this.state.locationName}</h1>
+        <ul>
+          {
+            this.state.weatherData.map((element) => {
+              return (
+                <ListItemWrapper key={element.date.unix()} text={element.date.format('ddd, MMMM Do')} />
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
   }
 }
 
--- src/client/app/helpers/WeatherApiHelper.js
+++ src/client/app/helpers/WeatherApiHelper.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import moment from 'moment';
 
 const API_KEY = '02da6f10a10d89d3cf2f42b249703986';
 
@@ -6,20 +7,30 @@ const parseParams = (cityInput) => cityInput.split(',');
 
 const getWeatherData = (input) => {
   const params = parseParams(input);
-  const weatherEndPoint = 'http://api.openweathermap.org/data/2.5/weather';
-  const query = {q: params.join(','), APIKEY: API_KEY};
+  const weatherEndPoint = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+  const query = {q: params.join(','), cnt: 5, APIKEY: API_KEY};
   return axios.get(weatherEndPoint, {
     params: query
   })
   .then(
     (response) => {
-      console.log(response.data);
-      console.log(response.status);
-      console.log(response.statusText);
-      console.log(response.headers);
-      console.log(response.config);
+      const data = response.data;
+      console.log(data);
+      let returnObj = {};
+      returnObj.cityName = data.city.name;
+      returnObj.weatherData = data.list.map((element) => {
+        return {
+          date: moment.unix(element.dt),
+          weatherDescription: element.weather.description,
+          weatherId: element.weather.id,
+          minTemp: element.temp.min,
+          maxTemp: element.temp.max,
+          humidity: element.humidity
+        }
+      });
+      return returnObj;
     })
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(`There was an error trying to retrieve weather data, message is: ${error}`));
 }
 
 export {getWeatherData};
